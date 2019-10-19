e414aa87f01b80981e80725f355cd08878bd4c32
Thomas <thms.cousin.pro@gmail.com>
2019-10-20 01:36:15 +0200

    [nouvelle page]
    Configuration de la page d'accueil : route, controleur et vue.

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('Homepage/homepage.html.twig');
+    }
+}
